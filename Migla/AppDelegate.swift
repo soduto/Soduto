30af979ea130dc44e78eb2f103eff1126abee50d
Giedrius Stanevicius <giedrius.stanevicius@gmail.com>
2016-07-06 22:00:09 +0300

    Initial Commit

---
+++ Migla/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Migla
+//
+//  Created by Admin on 2016-07-06.
+//  Copyright © 2016 Migla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
