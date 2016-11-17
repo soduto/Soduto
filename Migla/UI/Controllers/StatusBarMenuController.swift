8b7e21168d909c3646011d4b64e3896fd177d6da
Giedrius Stanevicius <giedrius.stanevicius@gmail.com>
2016-11-17 14:58:37 +0200

    Show available paired devices in status bar menu

--- Migla/UI/Controllers/StatusBarMenuController.swift
+++ Migla/UI/Controllers/StatusBarMenuController.swift
@@ -9,11 +9,10 @@
 import Foundation
 import AppKit
 
-public class StatusBarMenuController: NSObject {
+public class StatusBarMenuController: NSObject, NSMenuDelegate {
     
     @IBOutlet weak var statusBarMenu: NSMenu!
-    @IBOutlet weak var pairedDevicesItem: NSMenuItem!
-    @IBOutlet weak var unpairedDevicesItem: NSMenuItem!
+    @IBOutlet weak var availableDevicesItem: NSMenuItem!
     
     public var deviceDataSource: DeviceDataSource?
     
@@ -44,9 +43,42 @@ public class StatusBarMenuController: NSObject {
     }
     
     
+    // MARK: NSMenuDelegate
+    
+    public func menuNeedsUpdate(_ menu: NSMenu) {
+        if menu == self.statusBarMenu {
+            self.refreshMenuDeviceList()
+        }
+    }
+    
+    
     
     func refreshDeviceLists() {
         self.preferencesWindowController?.refreshDeviceList()
     }
     
+    func refreshMenuDeviceList() {
+        // remove old device items
+        
+        var item = self.statusBarMenu.item(withTag: InterfaceElementTags.availableDeviceMenuItem.rawValue)
+        while item != nil {
+            self.statusBarMenu.removeItem(item!)
+            item = self.statusBarMenu.item(withTag: InterfaceElementTags.availableDeviceMenuItem.rawValue)
+        }
+        
+        // add new device items
+        
+        let devices = self.deviceDataSource?.pairedDevices ?? []
+        guard devices.count > 0 else { return }
+        
+        var index = self.statusBarMenu.index(of: self.availableDevicesItem)
+        assert(index != -1, "availableDevicesItem expected to be item of statusBarMenu")
+        for device in devices {
+            let item = NSMenuItem(title: device.name, action: nil, keyEquivalent: "")
+            item.tag = InterfaceElementTags.availableDeviceMenuItem.rawValue
+            item.submenu = NSMenu()
+            index += 1
+            self.statusBarMenu.insertItem(item, at: index)
+        }
+    }
 }
